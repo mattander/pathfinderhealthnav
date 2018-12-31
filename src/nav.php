bee83a52ecb18ee9ab95af88fa7d946a842d2a67
Matt Anderson <matty_ander@hotmail.com>
2018-12-31 16:25:34 -0500

    added link for brand image

--- src/nav.php
+++ src/nav.php
@@ -1,6 +1,6 @@
 <nav id="mainNav" class="navbar navbar-expand-sm">
 
-  <a class="navbar-brand" href="#"><img src="<?php echo get_theme_file_uri('images/logo.png'); ?>" alt=""></a>
+  <a class="navbar-brand" href="/"><img src="<?php echo get_theme_file_uri('images/logo.png'); ?>" alt=""></a>
     <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
       <span class="navbar-toggler-icon">Menu</span>
     </button>
