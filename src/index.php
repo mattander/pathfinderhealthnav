b26f49feec5837411e7177abf754a7ef9fa3b4ef
Matt Anderson <matty_ander@hotmail.com>
2019-01-14 18:01:03 -0500

    content page layout fixes

--- src/index.php
+++ src/index.php
@@ -1,10 +1,16 @@
 <?php require('header.php'); ?>
 
 <div class="container mt-4">
-<h1><?php echo get_field('title'); ?></h1>
-<section id="main-content">
-  <?php echo get_field('content'); ?>
-</section>
+  <h1>
+    <?php echo get_field('title'); ?>
+  </h1>
+  <section id="main-content">
+    <div class="row">
+      <div class="col-lg-9 col-md-10">
+        <?php echo get_field('content'); ?>
+      </div>
+    </div>
+  </section>
 </div>
 
 <?php require('footer.php'); ?>
--- src/nav.php
+++ src/nav.php
@@ -1,21 +1,23 @@
 <nav id="mainNav" class="navbar navbar-expand-lg">
 
-  <a class="navbar-brand" href="/"><img src="<?php echo get_theme_file_uri('images/logo.png'); ?>" alt=""></a>
-  <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent"
-    aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-    <span class="nav-menu">Menu</span>
-  </button>
+  <div class="container">
+    <a class="navbar-brand" href="/"><img src="<?php echo get_theme_file_uri('images/logo.png'); ?>" alt=""></a>
+    <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent"
+      aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+      <span class="nav-menu">Menu</span>
+    </button>
 
-  <?php 
-  wp_nav_menu(array(
-    'theme_location' => 'primary',
-    'depth' => 2, // 1 = no dropdowns, 2 = with dropdowns.
-    'container' => 'div',
-    'container_class' => 'collapse navbar-collapse',
-    'container_id' => 'navbarSupportedContent',
-    'menu_class' => 'navbar-nav mr-auto',
-    'fallback_cb' => 'WP_Bootstrap_Navwalker::fallback',
-    'walker' => new WP_Bootstrap_Navwalker()
-  ));
-  ?>
+    <?php 
+    wp_nav_menu(array(
+      'theme_location' => 'primary',
+      'depth' => 2, // 1 = no dropdowns, 2 = with dropdowns.
+      'container' => 'div',
+      'container_class' => 'collapse navbar-collapse',
+      'container_id' => 'navbarSupportedContent',
+      'menu_class' => 'navbar-nav mr-auto',
+      'fallback_cb' => 'WP_Bootstrap_Navwalker::fallback',
+      'walker' => new WP_Bootstrap_Navwalker()
+    ));
+    ?>
+  </div>
 </nav>
