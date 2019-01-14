2c5e0a257788978bc3e266a4e350a8d79921fb65
Matt Anderson <matty_ander@hotmail.com>
2019-01-14 18:21:01 -0500

    fixed some typography

--- src/index.php
+++ src/index.php
@@ -1,12 +1,12 @@
 <?php require('header.php'); ?>
 
 <div class="container mt-4">
-  <h1>
-    <?php echo get_field('title'); ?>
-  </h1>
   <section id="main-content">
     <div class="row">
       <div class="col-lg-9 col-md-10">
+        <h1>
+          <?php echo get_field('title'); ?>
+        </h1>
         <?php echo get_field('content'); ?>
       </div>
     </div>
